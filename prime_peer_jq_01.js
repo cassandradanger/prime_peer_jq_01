19df01d7b0e5d8199033047bfa0c642ad7685e4d
Casie Lynch <cds.lynch@gmail.com>
2015-04-21 08:09:29 -0500

    cleaned up code

--- prime_peer_jq_01.js
+++ prime_peer_jq_01.js
@@ -5,31 +5,29 @@ function randomNumber(min, max) {
 }
 
 
-var x = "";
-var first = 0;
-var last =  0;
 var feFirst = ["Olga", "Helga", "Lena"]; 
 var maFirst = ["Sven", "Hans", "Ollie"];
 var family = ["Anderson", "Eriksen", "Bjornstad", "Engelstad", "Rosdahl"];
 
 function Human(age, sex, weight){
 	this.age = age;
-	this.sex = sex;
-		if (this.sex == 1){
-			x = "male";
-			first = maFirst[randomNumber(0,2)];
-			last = family[randomNumber(0,4)]
-		}else{
-			x = "female";
-			first = feFirst[randomNumber(0,2)];
-			last = family[randomNumber(0,4)]
-		};
+
+	if (sex == 1){
+		this.sex = "male";
+		this.first = maFirst[randomNumber(0,2)];
+		this.last = family[randomNumber(0,4)]
+	}else{
+		this.sex = "female";
+		this.first = feFirst[randomNumber(0,2)];
+		this.last = family[randomNumber(0,4)]
+	};
 	this.weight = weight;
 }
  
 $(document).ready(function (){
 	$("#randomPerson").on("click", function (){
 		var Human1 = new Human(randomNumber(1,100), randomNumber(1,2), randomNumber(101,200));
-		$("#start").prepend("<li>" + first + " " + last + "<br/>Age: " + Human1.age + ", Sex: " + x + ", Weight: " + Human1.weight + "</li><br/>")
+		$("#start").prepend("<li>" + Human1.first + " " + Human1.last + "<br/>Age: " + Human1.age + ", Sex: " + Human1.sex
+		 + ", Weight: " + Human1.weight + "</li><br/>")
 	});
 });
